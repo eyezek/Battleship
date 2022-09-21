073877629073b3cf3951fd7cce0f81a989cb2dc5
eyezek <71928231+eyezek@users.noreply.github.com>
2022-09-20 20:22:53 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
-const shipFactory = ((length) => {
+playerSquares = document.getElementsByClassName("playersquares");
+computerSquares = document.getElementsByClassName("computersquares");
+
+const createPlayerSquares = () => {
+  for (let i = 0; i < 100; i++) {
+    let square = document.createElement("div");
+    square.className = "playerSquares";
+    square.setAttribute("id", "square");
+
+    document.getElementById("playergameboard").appendChild(square);
+  }
+};
+
+const createComputerSquares = () => {
+  for (let i = 0; i < 100; i++) {
+    let square = document.createElement("div");
+    square.className = "computerSquares";
+    square.setAttribute("id", "square");
+
+    document.getElementById("computergameboard").appendChild(square);
+  }
+};
+
+const shipFactory = ((name, length) => {
   const shipLength = length;
   let hitCount = 0;
   const hit = () => {
@@ -12,6 +35,7 @@ const shipFactory = ((length) => {
   };
 
   return {
+    name,
     shipLength,
     hitCount,
     hit,
@@ -31,15 +55,49 @@ const gameBoard = (() => {
     return board;
   };
 
-  const placeShip = (length, location, hitbox) => {
-    shipFactory(length);
+  const placeShip = (length, name, board) => {
+    shipFactory(name, length);
+    if (shipLength < 1) {
+      return;
+    } else
+      for (let i = 0; i <= shipLength; i++) {
+        board.splice(x, shipLength, "1");
+      }
+    return board;
   };
 
-  return { populateBoard, board, placeShip };
+  let missedAttacks = 0;
+
+  const receiveAttack = (board) => {
+    if (board[x][y] === "0") {
+      board[x][y];
+      return missedAttacks + 1;
+    } else {
+      shipFactory.hit();
+    }
+  };
+
+  const checkIfAllSunk = (board) => {
+    for (let i = 0; i <= board.length; i++) {
+      if (board[x] === "0" && board[y] === "0") {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  };
+
+  return { populateBoard, board, placeShip, receiveAttack, checkIfAllSunk };
 })();
 
-const playerFactory = (name) => {
+const playerFactory = ((name) => {
   return name;
-};
+})();
+
+const game = () => {};
+
+createPlayerSquares();
+
+createComputerSquares();
 
 gameBoard.populateBoard;
